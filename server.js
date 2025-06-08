72b555f325b7fc4834d065b014b04f6fc6f44ba5
Om Kadu <kaduom444@gmail.com>
2025-06-08 22:17:56 +0530

    bug fix

--- server.js
+++ server.js
@@ -17,20 +17,20 @@ app.use(express.static('public'));
 
 
 // Database connection
-const db = mysql.createConnection({
+const db = mysql.createPool({
     host: process.env.HOST,
     user: process.env.USER,
     password: process.env.PASSWORD,
     database: process.env.DATABASE
 });
 
-db.connect(err => {
-    if (err) {
-        console.error('Error connecting to MySQL:', err);
-        return;
-    }
-    console.log('Connected to MySQL database');
-});
+// db.connect(err => {
+//     if (err) {
+//         console.error('Error connecting to MySQL:', err);
+//         return;
+//     }
+//     console.log('Connected to MySQL database');
+// });
 
 // Utility Function: Handle Errors
 const handleError = (err, res, message = 'An error occurred') => {
